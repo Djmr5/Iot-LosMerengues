7ecb5405878591af61a378e7368151c4c0ca6522
Morkigloxx <102175762+Morkigloxx@users.noreply.github.com>
2022-11-25 13:42:15 -0600

    Update rescueDB.sql

--- sqldatabase/rescueDB.sql
+++ sqldatabase/rescueDB.sql
@@ -5,6 +5,6 @@ CREATE DATABASE rescueDB
 CREATE TABLE messages(
     nodeID INT PRIMARY KEY,
     severity VARCHAR(255),
-    longitude VARCHAR(255),
-    latitude VARCHAR(255)
+    latitude VARCHAR(255),
+    longitude VARCHAR(255)
 );
