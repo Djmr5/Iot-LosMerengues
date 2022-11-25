b1e9349be0087ae1fd3ed8cdaad7570afc478f9a
Morkigloxx <102175762+Morkigloxx@users.noreply.github.com>
2022-11-25 11:31:28 -0600

    Create rescueDB.sql

---
+++ sqldatabase/rescueDB.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE rescueDB
+
+/*USE rescueDB */
+
+CREATE TABLE messages(
+    nodeID INT PRIMARY KEY,
+    severity VARCHAR(255),
+    longitude VARCHAR(255),
+    latitude VARCHAR(255)
+);
